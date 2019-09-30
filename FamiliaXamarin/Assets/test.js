e0300a6a4b2f695be04d015fdc188a5087195474
Patricia Mic <micpatricia97@gmail.com>
2019-09-30 14:58:19 +0300

    Games - Ball GYRO
    Scene:
    -init: sets all elements in page, animation for starting, calls the update
    -update: a loop which ends when game is over
    -finish: when the ball is in the arrival  area, bouncing animation
    
    -play again  button calls the init function

--- FamiliaXamarin/Assets/test.js
+++ FamiliaXamarin/Assets/test.js
@@ -1,139 +1,196 @@
-﻿var dimensionsString;
-var movementX;
-var movementY;
-var speedX;
-var speedY;
-var isOver;
-var refreshIntervalId;
+﻿const scene = {
+    init: '',
+    update: '',
+    finish: ''
+}
 
-function initGame() {
-    $("#reset").css({ display: 'none' });
+let dimensionsString;
+let movementX;
+let movementY;
+let speedX;
+let speedY;
+let isOver;
+let isStarted;
+let refreshIntervalId;
 
-    $.keyframe.define([
-        {
-            name: 'ball-move',
-            '0%': {
-                'margin-left': 50 + 'px',
-                'margin-top': 0 + 'px'
-            },
-            '50%': {
-                'margin-left': 50 + 'px',
-                'margin-top': 100 + 'px'
-            },
-            '100%': {
-                'margin-left': 50 + 'px',
-                'margin-top': 200 + 'px'
-            }
+$.keyframe.define([
+    {
+        name: 'ball-move',
+        '0%': {
+            'margin-left': 50 + 'px',
+            'margin-top': 0 + 'px'
+        },
+        '100%': {
+            'margin-left': 50 + 'px',
+            'margin-top': 200 + 'px'
         }
-    ]);
+    },
+    {
+        name: 'bounce',
+        '0%': {
+            transform: 'translateY(0);'
+        },
+        '50%': {
+            transform: 'translateY(-60px);'
+        },
+        '100%': {
+            transform: 'translateY(0);'
+        }
+    }
+]);
+
+scene.init = function() {
+    isStarted = false;
+    isOver = false;
+
+    $("#reset").css({ display: 'none' });
+    $(".player").css({ display: 'none' });
+    $(".arrival").css({ top: '0px', left: '50px', position: 'absolute' });
 
     $('.arrival').playKeyframe({
         name: 'ball-move',
         duration: '2s',
         timingFunction: 'ease-out',
-        'fill-mode': 'forwards'
+        'fill-mode': 'forwards',
+        complete: function () {
+            $(".player").css({ top: '0px', left: '0px', position: 'absolute', display: 'block', animation: 'none' });
+            isStarted = true;
+            sendMessage("Animation is completed. Starting the game .... ");
+            sendMessage("Loop started..");
+            dimensionsString = JSHandler.getScreenDimension();
+            movementX = 0;
+            movementY = 0;
+            speedX = 0;
+            speedY = 0;
+            refreshIntervalId = setInterval(scene.update, 10);
+        }
     });
-
-    dimensionsString = JSHandler.getScreenDimension();
-    movementX = 0;
-    movementY = 0;
-    speedX = 0;
-    speedY = 0;
-    isOver = false;
-    refreshIntervalId = 'none';
     sendMessage("game loaded");
 }
 
-var loop = function () {
-    if (isOver == false) {
-        var values = JSHandler.getXYFromGyro();
-        var currentX = Number(values.split("/")[0]);
-        var currentY = Number(values.split("/")[1]);
+scene.update = function () {
+    if (isOver === false && isStarted === true) {
+        let values = JSHandler.getXYFromGyro();
+        let currentX = Number(values.split("/")[0]);
+        let currentY = Number(values.split("/")[1]);
 
         $(".player").parent().css({ position: 'relative' });
-        $(".arrival").css({ top: '200px', left: '50px', position: 'absolute' });
+        $(".arrival").css({ position: 'absolute' });
+
+        const player = {
+            width: $(".player").outerWidth(),
+            height: $(".player").outerHeight(),
+            position: {
+                left: $(".player").position().left,
+                top: $(".player").position().top
+            }
+        }
 
         if (currentX !== 0 && currentY !== 0) {
 
             if (currentX > 0.3 || currentX < -0.3) {
-                movementX = (speedX * currentX) + $(".player").position().left;
+                movementX = (speedX * currentX) + player.position.left;
             }
-
             if (currentY < -0.3 || currentY > 0.3) {
-                movementY = (speedY * currentY) + $(".player").position().top;
+                movementY = (speedY * currentY) + player.position.top;
             }
-
-            if (movementY <= Number($(window).height()) - 80 &&
-                movementY >= 0 &&
-                movementX <= Number($(window).width()) - 80 &&
-                movementX >= 0) {
+            if (movementY <= Number($(window).height()) - player.height && movementY >= 0 &&
+                movementX <= Number($(window).width()) - player.width && movementX >= 0) {
                 $(".player").css({ left: movementX, top: movementY, position: 'absolute' });
             }
 
             sendMessage("moveX " + movementX + " speedX: " + (speedX * currentX));
             sendMessage("moveY " + movementY + " speedY: " + (speedY * currentY));
-            sendMessage("Player position top: " + $(".player").position().top + " left: " + $(".player").position().left);
+            sendMessage("Player position top: " + player.position.top + " left: " + player.position.left);
 
             if (speedX < 10) {
                 speedX++;
             }
-
             if (speedY < 10) {
                 speedY++;
             }
-
         } else {
             speedX = 0;
             speedY = 0;
             sendMessage("not moving");
         }
-
         if (checkForFinish() === true) {
             isOver = true;
         }
     } else {
-        $("#reset").css({ display: 'block' });
-        alert("YOU WON!");
-        sendMessage("Interval id: " + refreshIntervalId);
-        clearInterval(refreshIntervalId);
+        scene.finish();
     }
 };
 
+scene.finish = function() {
+    sendMessage("finish called");
+    $("#reset").css({ display: 'block' });
+    displayPlayerForFinish();
+    sendMessage("Interval id: " + refreshIntervalId);
+    clearInterval(refreshIntervalId);
+}
+
+
+function displayPlayerForFinish() {
+    const arrivalPlace = {
+        width: $(".arrival").outerWidth(),
+        height: $(".arrival").outerHeight(),
+        center: {
+            x: $(".arrival").offset().left,
+            y: $(".arrival").offset().top
+        }
+    };
+    $(".player").css({ left: arrivalPlace.center.x, top: arrivalPlace.center.y, position: 'absolute' });
+    $('.player').playKeyframe({
+        name: 'bounce',
+        duration: '2s',
+        complete: function () { }
+    });
+}
 
 function checkForFinish() {
-    var offset = $(".player").offset();
-    var playerWidth = $(".player").outerWidth();
-    var playerHeight = $(".player").outerHeight();
-    var playerCenterX = offset.left + playerWidth / 2;
-    var playerCenterY = offset.top + playerHeight / 2;
 
-    var offsetA = $(".arrival").offset();
-    var arrivalWidth = $(".arrival").outerWidth();
-    var arrivalHeight = $(".arrival").outerHeight();
-    var arrivalCenterX = offsetA.left + arrivalWidth / 2;
-    var arrivalCenterY = offsetA.top + arrivalHeight / 2;
-
-    sendMessage("Center Player XY: " + playerCenterX + " / " + playerCenterY);
-    sendMessage("Center Arrival XY: " + arrivalCenterX + " / " + arrivalCenterY);
-
-    if (Math.floor(playerCenterX) === Math.floor(arrivalCenterX) &&
-        Math.floor(playerCenterY) === Math.floor(arrivalCenterY)) {
-        return true;
-    } else {
-        return false;
+    const player = {
+        width: $(".player").outerWidth(),
+        height: $(".player").outerHeight(),
+        center: {
+            x: $(".player").offset().left + ($(".player").outerWidth() / 2),
+            y: $(".player").offset().top + ($(".player").outerHeight() / 2)
+        }
     }
+
+    const arrivalPlace = {
+        width: $(".arrival").outerWidth(),
+        height: $(".arrival").outerHeight(),
+        center: {
+            x: $(".arrival").offset().left + ($(".arrival").outerWidth() / 2),
+            y: $(".arrival").offset().top + ($(".arrival").outerHeight() / 2)
+        }
+    };
+
+    sendMessage("Center Player XY: " + player.center.x + " / " + player.center.x);
+    sendMessage("Center Arrival XY: " + arrivalPlace.center.x + " / " + arrivalPlace.center.y);
+
+    if (Math.floor(player.center.x) < Math.floor(arrivalPlace.center.x + (arrivalPlace.width / 2)) &&
+        Math.floor(player.center.x) > Math.floor(arrivalPlace.center.x - (arrivalPlace.width / 2)) &&
+        Math.floor(player.center.y < Math.floor(arrivalPlace.center.y + (arrivalPlace.height / 2))) &&
+        Math.floor(player.center.y) > Math.floor(arrivalPlace.center.y - (arrivalPlace.height / 2))) {
+        sendMessage("Player floor: " + Math.floor(player.center.x) + " / " + Math.floor(player.center.y));
+        sendMessage("Arrival X: " + Math.floor(arrivalPlace.center.x) + " / " + Math.floor(arrivalPlace.center.x - arrivalPlace.width));
+        sendMessage("Arrivale Y: " + Math.floor(arrivalPlace.center.y) + " / " + Math.floor(arrivalPlace.center.y - arrivalPlace.height));
+        return true;
+    }
+
+    return false;
 }
 
 function playAgain() {
     sendMessage("reloading game...");
-    isOver = false;
-    initGame();
-    refreshIntervalId = setInterval(loop, 10);
+    scene.init();
 }
 
 function getFromAndroid() {
-    var myVar = JSHandler.getFromAndroid();
-    alert(myVar);
+    alert(JSHandler.getFromAndroid());
 }
 
 function sendMessage(message) {
@@ -141,7 +198,6 @@ function sendMessage(message) {
 }
 
 $(document).ready(function () {
-    initGame();
-    refreshIntervalId = setInterval(loop, 10);
+    scene.init();
 });
 
