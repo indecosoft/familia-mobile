bd4ac05a91c2dcdfcea7d6347c292853ed227cb6
Patricia Mic <micpatricia97@gmail.com>
2020-03-19 11:11:37 +0200

    Joc 2 - Step 1 - Done
    - position solved

--- FamiliaXamarin/Assets/joc2/game.js
+++ FamiliaXamarin/Assets/joc2/game.js
@@ -18,8 +18,20 @@ scene.init = function () {
     generateDashedElements(4);
     generateBorderedElements(6);
 
-    $(".round-green").css({ top: 80, left: 750, position: 'absolute', display: 'block' });
-    $(".round-grey").css({ top: 120, left: 800, position: 'absolute', display: 'block' });
+    $(".round-green").css({
+        top: 80,
+        left: 750,
+        position: 'absolute',
+        display: 'block'
+    });
+
+    $(".round-grey").css({
+        top: 120,
+        left: 800,
+        position: 'absolute',
+        display: 'block'
+    });
+
     scene.refreshIntervalId = setInterval(scene.update, 5);
 }
 
@@ -37,7 +49,12 @@ function generateDashedElements(nrOfItems) {
         let item = $('<div id="dashed' + i + '"class="dashed"></div>');
         centerX += 100;
 
-        item.css({ left: centerX, top: centerY, display: 'block', position: 'absolute' })
+        item.css({
+            left: centerX,
+            top: centerY,
+            display: 'block',
+            position: 'absolute'
+        })
             .droppable({
                 drop: function (event, ui) {
                     ui.helper.data('dropped', true);
@@ -68,7 +85,13 @@ function generateBorderedElements(nrOfItems) {
         let item = $('<div id="child' + i + '" class="child"></div>');
         centerX += 100;
 
-        item.css({ left: centerX, top: centerY, display: 'block', position: 'absolute', 'background-image': 'url("images/pui' + i + '.png")' })
+        item.css({
+            left: centerX,
+            top: centerY,
+            display: 'block',
+            position: 'absolute',
+            'background-image': 'url("images/pui' + i + '.png")'
+        })
             .draggable({
                 revert: false,
                 start: function (event, ui) {
@@ -130,36 +153,61 @@ function onDropInside(dashedElement, uiElement) {
         }
     };
 
-    const error = 7;
-    uiElement.css({ left: dashed.center.x - (child.width / 2) - error, top: dashed.center.y - (child.height / 2) - error, position: 'absolute' });
-    updateCurrentPostion(uiElement, child.center.x, child.center.y);
-
-    //TODO fix - when you move a dashed into another dashed element, the behaviour is not ok
+    let isMovedBetweenDashedElements = false;
+    const idUiElement = getPosition(uiElement, children);
+    if (children[idUiElement].currentPosition.x != children[idUiElement].oldPosition.x &&
+        children[idUiElement].currentPosition.y != children[idUiElement].oldPosition.y) {
+        isMovedBetweenDashedElements = true;
+    }
 
     const idElement = getPosition(dashedElement, dashedElements);
+    setChildToDashedPosition(uiElement, dashed, child, idElement);
+    removeStinkyItem(isMovedBetweenDashedElements, idElement, uiElement);
+}
 
-    if (dashedElements[idElement].occupied.element == uiElement) {
-        console.log('same element');
-    } else {
+function removeStinkyItem(isMovedBetweenDashedElements, idElement, uiElement) {
+    if (isMovedBetweenDashedElements == true) {
+        for (let i = 0; i < dashedElements.length; i++) {
+            if (i != idElement) {
+                if (dashedElements[i].occupied.element == uiElement) {
+                    dashedElements[i].occupied.element = '';
+                }
+            }
+        }
+    }
+}
+
+function setChildToDashedPosition(uiElement, dashed, child, idElement) {
+    const error = 7;
+    uiElement.css({
+        left: dashed.center.x - (child.width / 2) - error,
+        top: dashed.center.y - (child.height / 2) - error,
+        position: 'absolute'
+    });
+    updateCurrentPostion(uiElement, child.center.x, child.center.y);
+    if (dashedElements[idElement].occupied.element != uiElement) {
         try {
-            setElementToOldPosition(dashedElements[idElement].occupied.element, getPosition(dashedElements[idElement].occupied.element, children));
+            setElementToOldPosition(dashedElements[idElement].occupied.element,
+                getPosition(dashedElements[idElement].occupied.element, children));
         } catch (e) { }
         dashedElements[idElement].occupied.element = uiElement;
-
     }
 }
 
 function onDropOutside(ui) {
     const idElement = getPosition(ui.helper, children);
     if (idElement != -1) {
-        updateCurrentPostion(ui.helper, children[idElement].oldPosition.x, children[idElement].oldPosition.y);
+        updateCurrentPostion(ui.helper,
+            children[idElement].oldPosition.x,
+            children[idElement].oldPosition.y);
+
         setElementToOldPosition(ui.helper, idElement);
 
         for (let i = 0; i < dashedElements.length; i++) {
             try {
                 let idx = getPosition(dashedElements[i].occupied.element, children);
-                if (children[idx].oldPosition.x == children[idx].currentPosition.x && children[idx].oldPosition.y == children[idx].currentPosition.y) {
-                    console.log('Important 3 item is still in the dashedelements but his location is at old position');
+                if (children[idx].oldPosition.x == children[idx].currentPosition.x &&
+                    children[idx].oldPosition.y == children[idx].currentPosition.y) {
                     dashedElements[i].occupied.element = '';
                 }
             } catch (e) { }
@@ -177,7 +225,11 @@ function setElementToOldPosition(ui, idElement) {
         }
     };
     const error = 7;
-    ui.css({ left: child.center.x - (child.width / 2) - error, top: child.center.y - (child.height / 2) - error, position: 'absolute' });
+    ui.css({
+        left: child.center.x - (child.width / 2) - error,
+        top: child.center.y - (child.height / 2) - error,
+        position: 'absolute'
+    });
 }
 
 function updateCurrentPostion(uiElement, x, y) {
